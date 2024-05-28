5b5eae2e67f401517d781d80c68a08371fc83550
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-05-28 15:28:42 +0200

    1

---
+++ src/DefaultCart.java
@@ -0,0 +1,28 @@
+public class DefaultCart implements Cart{
+    // <write your code here>
+
+    @Override
+    public boolean isEmpty() {
+        // <write your code here>
+
+        return true;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        // <write your code here>
+    }
+
+    @Override
+    public Product[] getProducts() {
+        // <write your code here>
+
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        // <write your code here>
+    }
+
+}
---
+++ src/DefaultProduct.java
@@ -0,0 +1,10 @@
+public class DefaultProduct {
+    private int id;
+    private String productName;
+    private String categoryName;
+    private double price;
+
+    public DefaultProduct() {
+
+    }
+}
