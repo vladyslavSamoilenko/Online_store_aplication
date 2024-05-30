8965c953835d0aee23c9160408ac16d302730d74
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-05-30 20:19:29 +0200

    Created sign up menu

---
+++ src/menu/impl/SingUpMenu.java
@@ -0,0 +1,50 @@
+package menu.impl;
+
+import configs.ApplicationContext;
+import enteties.User;
+import enteties.impl.DefaultUser;
+import menu.Menu;
+import services.UserManagementService;
+import services.impl.DefaultUserManagementService;
+
+import java.util.Scanner;
+
+public class SingUpMenu implements Menu {
+    private UserManagementService userManagementService;
+    private ApplicationContext context;
+
+    {
+        userManagementService = DefaultUserManagementService.getInstance();
+        context = ApplicationContext.getInstance();
+    }
+    @Override
+    public void start() {
+        printMenuHeader();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter your first name");
+        String firstName = sc.next();
+        System.out.println("Please, enter your last name");
+        String lastName = sc.next();
+        System.out.println("Please , enter your password");
+        String password = sc.next();
+        System.out.println("Please, enter your email");
+        String userEmail = sc.nextLine();
+
+        User user = new DefaultUser(firstName, lastName, password, userEmail);
+
+        String errorMessage = userManagementService.registerUser(user);
+        if (errorMessage == null || errorMessage.isEmpty()){
+            context.getLoggedInUser(user);
+            System.out.println("Nwq user is created");
+        }else {
+            System.out.println(errorMessage);
+        }
+
+    }
+
+    @Override
+    public void printMenuHeader() {
+        System.out.println("*** SIGN UP MENU ***");
+    }
+}
